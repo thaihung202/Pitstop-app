150e4e7457fdfc251d27265a450602a5a81955bf
Hưng Trần Thái <21522128@ms.uit.edu.vn>
2025-02-09 20:40:34 +0700

    Update code

--- src/k8s/istio/disable-default-istio-injection.ps1
+++
@@ -1,7 +0,0 @@
-# In order to use the istio-injection annotation only on specific deployments within 
-# the pitstop namespace, we need to disable automatic injection and enable it explicitly 
-# on the namespace (see start-all script).
-
-$config = kubectl get configmap istio-sidecar-injector -o yaml -n istio-system
-$config = $config -replace 'policy: enabled$', 'policy: disabled'
-$config | kubectl apply -n istio-system -f -
